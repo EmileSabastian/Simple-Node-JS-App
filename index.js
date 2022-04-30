b813f867c4688a4f03f4f57339f4e8ce8328a1dc
EmileSabastian <32434605+EmileSabastian@users.noreply.github.com>
2022-04-30 12:19:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const port = 3000;
 
-app.get('/', (req, res) => res.send("Welcome to Tutsmake first Node JS app! Keep things Jiggy ;-) "));
+//app.get('/', (req, res) => res.send("Welcome to Tutsmake first Node JS app! Keep things Jiggy ;-) "));
+app.get("/" + "modal.html", gotoIndex);
+app.route("/");
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
